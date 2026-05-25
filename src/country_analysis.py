227764ee1d5d16caa460fbd0c5358761589ce7af
cesar-asg <cesaraugusto.sierragarzon@gmail.comm>
2026-05-25 18:54:47 -0400

    add function skeletons for country analysis

---
+++ src/country_analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("supply_delivery_history.csv")
+print(df.columns)
+print(df.head())
+
+def summarize_by_country(df):
+    pass
+
+def shipments_count_by_country(df):
+    pass
