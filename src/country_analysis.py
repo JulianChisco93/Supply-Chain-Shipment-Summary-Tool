795909e31c8b8790f799353142dc4725d432658d
cesar-asg <cesaraugusto.sierragarzon@gmail.comm>
2026-05-26 20:11:38 -0400

    implement basic country analysis functions

--- src/country_analysis.py
+++ src/country_analysis.py
@@ -7,7 +7,18 @@ print(df.head())
 print(df.info())
 
 def summarize_by_country(df):
-    pass
+    return df.groupby("Country").agg({
+        "Line Item Quantity": "sum",
+        "Line Item Value": "sum"
+    }).reset_index()
 
 def shipments_count_by_country(df):
-    pass
+    return df.groupby("Country").size().reset_index(name="Total Shipments")
+
+
+import pandas as pd
+
+df = pd.read_csv("supply_delivery_history_clean.csv")
+
+print(summarize_by_country(df).head(10))
+print(shipments_count_by_country(df).head(10))
